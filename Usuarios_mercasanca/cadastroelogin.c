0f06f1417db9f8b108d2d09f2010793ac24560e5
brunamata <brunamata@usp.br>
2022-06-26 19:23:11 -0300

    lab
    adicionando usuarios ou coisas assim

--- Usuarios_mercasanca/cadastroelogin.c
+++ Usuarios_mercasanca/cadastroelogin.c
@@ -35,7 +35,7 @@ void cadastro(){
     fclose(cadastramento);
 }
 
-void login(){
+int login(){
     char nome[limitedeusuarios][100], email[limitedeusuarios][100], senha[limitedeusuarios][15];
 
     char emaillogin[100], senhalogin[15];
@@ -78,38 +78,47 @@ int x = 0;
         printf("houve algum erro, confira se seu email e senha estao corretos\n");
 
     fclose(arqlogin);
+
+    return x;
     
+}
+
+void teladelogin(){
+    int resposta, loginn;
+    char temp;
+
+printf("------Bem vindo!------\n");
+printf("pressione: \n");
+printf("0 - encerrar\n");
+printf("1 - cadastro\n");
+printf("2 - login\n");
+printf("deseja fazer cadastro ou login? ");
+scanf("%d", &resposta);
+scanf("%c", &temp);
+
+if(resposta == 1)
+    cadastro();
+else if(resposta == 2)
+    loginn = login();
+
+    while(resposta != 0 && loginn != 1){
+        printf("deseja fazer cadastro ou login? ");
+        scanf("%d", &resposta);
+        scanf("%c", &temp);
+
+        if(resposta == 1)
+            cadastro();
+        else if(resposta == 2){
+            loginn = login();
+        }
+    }
+
 }
 
 
 // int main(){
-//     int resposta;
-//     char temp;
 
-// printf("------Bem vindo!------\n");
-// printf("pressione: \n");
-// printf("0 - encerrar\n");
-// printf("1 - cadastro\n");
-// printf("2 - login\n");
-// printf("deseja fazer cadastro ou login? ");
-// scanf("%d", &resposta);
-// scanf("%c", &temp);
-
-// if(resposta == 1)
-//     cadastro();
-// else if(resposta == 2)
-//     login();
-
-//     while(resposta != 0){
-//         printf("deseja fazer cadastro ou login? ");
-//         scanf("%d", &resposta);
-//         scanf("%c", &temp);
-
-//         if(resposta == 1)
-//             cadastro();
-//         else if(resposta == 2)
-//             login();
-//     }
+//teladelogin();
 
 
 
